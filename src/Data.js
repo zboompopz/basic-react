8009179eeeb05e52082a9071d07136cbc0fdbf14
zboompopz <zboompopz@hotmail.com>
2020-07-17 09:18:31 +0700

    first commit

---
+++ src/Data.js
@@ -0,0 +1,24 @@
+export default {
+    products: [
+        {
+            id:1,
+            name:'CPU',
+            brand:'Intel',
+            price: 3000,
+            rating: 4.5,
+            numStock: 100,
+            numReview: 30,
+            image: '/images/p1.jpg'
+        },
+        {
+            id:2,
+            name:'RAM',
+            brand:'Kington',
+            price: 1500,
+            rating: 3.0,
+            numStock: 50,
+            numReview: 20,
+            image: '/images/p2.jpg'
+        }
+    ]
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,6 @@ import {
     Nav,
     NavItem,
     NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    NavbarText
 } from 'reactstrap';
 
 
@@ -24,28 +19,28 @@ const Header = (props) => {
         <div>
 
 
-            <Navbar color="light" light expand="md">
-                <NavbarBrand> <Link to="/">Wutthinun</Link></NavbarBrand>
+            <Navbar color="warning" dark expand="md">
+                <NavbarBrand tag={Link} to="/">Wutthinun</NavbarBrand>
                 <NavbarToggler onClick={toggle} />
                 <Collapse isOpen={isOpen} navbar>
                     <Nav className="mr-auto" navbar>
                         <NavItem>
-                            <NavLink><Link to="/about"> เกี่ยวกับเรา </Link></NavLink>
+                            <NavLink tag={Link} to="/about"> เกี่ยวกับเรา </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink><Link to="/contact"> ติดต่อเรา </Link></NavLink>
+                            <NavLink tag={Link} to="/contact"> ติดต่อเรา </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink><Link to="/profile"> ข้อมูลส่วนตัว </Link></NavLink>
+                            <NavLink tag={Link} to="/profile"> ข้อมูลส่วนตัว </NavLink>
                         </NavItem>
                     </Nav>
                     <Nav className="ml-auto" nabar>
                         <NavItem>
-                            <NavLink><Link to="/signup"> เข้าสู่ระบบ </Link></NavLink>
+                            <NavLink tag={Link} to="/signup"> เข้าสู่ระบบ </NavLink>
                         </NavItem>
 
                         <NavItem>
-                            <NavLink><Link to="/signin"> สมัครสมาชิก </Link></NavLink>
+                            <NavLink tag={Link} to="/signin"> สมัครสมาชิก </NavLink>
                         </NavItem>
                     </Nav>
                 </Collapse>
